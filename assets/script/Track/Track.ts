5947d26c2997d9945636e52737792a6c242b139b
SamridhG <samridh1998@gmail.com>
2023-06-26 21:45:34 +0530

    Gameplay Scene Integrated

---
+++ assets/script/Track/Track.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Track')
+export class Track extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
