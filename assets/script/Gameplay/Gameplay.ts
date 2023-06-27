e9a6a785b94b15aac7c521a24c53ffc55f0ebecf
SamridhG <samridh1998@gmail.com>
2023-06-28 01:04:04 +0530

    Player Car Movement Integrated

--- assets/script/Gameplay/Gameplay.ts
+++ assets/script/Gameplay/Gameplay.ts
@@ -1,14 +1,47 @@
 import { _decorator, Component, Node } from 'cc';
+import { PlayerCar } from '../Player/PlayerCar';
 const { ccclass, property } = _decorator;
 
 @ccclass('Gameplay')
 export class Gameplay extends Component {
+    CarLane:number=0;
+    @property({type:Node})CarRoot=null;
+    TouchStartPosition;
+    TouchEndPosition;
     start() {
 
     }
+registerEvent(){
+    console.log("Event Register")
+    this.node.on(Node.EventType.TOUCH_MOVE,()=>{
 
+    },this)
+    this.node.on(Node.EventType.TOUCH_CANCEL,this.touchCancel,this)
+    this.node.on(Node.EventType.TOUCH_END,this.touchCancel,this)
+    this.node.on(Node.EventType.TOUCH_START,this.touchStart,this)
+}
+touchCancel(event){
+    this.TouchEndPosition=event.getLocation();
+    if(this.TouchEndPosition.x-this.TouchStartPosition.x>0){
+          this.CarLane++;
+          if(this.CarLane>1){
+            this.CarLane=1;
+          }
+         
+    }else if(this.TouchEndPosition.x-this.TouchStartPosition.x<0){
+        this.CarLane--;
+        if(this.CarLane<-1){
+          this.CarLane=-1;
+        }
+    }
+    this.CarRoot.getComponent(PlayerCar).setPositionCar(this.CarLane);
+}
+touchStart(event){
+    this.TouchStartPosition=event.getLocation();
+}
     update(deltaTime: number) {
         
     }
 }
-
+
+
--- assets/script/Player/PlayerCar.ts
+++ assets/script/Player/PlayerCar.ts
@@ -1,6 +1,7 @@
-import { _decorator, Component, Node, Sprite, tween } from 'cc';
+import { _decorator, Component, lerp, Node, Script, Sprite, tween, Vec3 } from 'cc';
 import { ResourceManager } from '../Manager/ResourceManager';
 import { PlayerInfo } from './PlayerInfo';
+import { Gameplay } from '../Gameplay/Gameplay';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerCar')
@@ -9,6 +10,7 @@ export class PlayerCar extends Component {
     @property({type:Node})MiddleNode=null;
     @property({type:Node})LeftNode=null;
     @property({type:Node})RightNode=null;
+    @property({type:Node})Canvas=null;
     ResourceLoad:ResourceManager=null;
     PlayerInfo:PlayerInfo=null;
      onLoad() {
@@ -21,7 +23,37 @@ export class PlayerCar extends Component {
         this.Car.getComponent(Sprite).spriteFrame=this.ResourceLoad.getCar(this.PlayerInfo.MyCar);  
     }
     carStartPosition(){
-        tween(this.Car).to(1.5,{position:this.MiddleNode.getPosition()}).start();
+        tween(this.Car).to(1.5,{position:this.MiddleNode.getPosition()}).call(()=>{
+            this.Canvas.getComponent(Gameplay).registerEvent();
+        }).start();
+    }
+    setPositionCar(number){
+        switch (number){
+            case -1:
+                this.setCarPositionLeft();
+                break;
+            case 0:
+                this.setCarPositionMiddle();
+                break;
+            case 1:
+                this.setCarPositionRight();
+                break;
+
+        }
+    }
+    setCarPositionLeft(){
+    // this.Car.setPosition(this.LeftNode.getPosition())
+    this.Car.angle=30;
+    tween(this.Car).to(0.1,{position:this.LeftNode.getPosition()}).call(()=>{ this.Car.angle=0;}).start();
+    }
+    setCarPositionRight(){
+       // this.Car.setPosition(this.RightNode.getPosition())
+       this.Car.angle=-30;
+        tween(this.Car).to(0.1,{position:this.RightNode.getPosition()}).call(()=>{ this.Car.angle=0;}).start();
+    }
+    setCarPositionMiddle(){
+        tween(this.Car).to(0.1,{position:this.MiddleNode.getPosition()}).start();
+       // this.Car.setPosition(this.MiddleNode.getPosition())
     }
     start() {
         
